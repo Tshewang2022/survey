e6119da5d1ef35023c93b199846bf187cdb21819
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-05-25 14:27:55 +0600

    back to the process

--- laravel-survey/vue/src/components/DefaultLayout.vue
+++ laravel-survey/vue/src/components/DefaultLayout.vue
@@ -77,13 +77,12 @@
                                         class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                     >
                                         <MenuItem
-                                            v-for="item in userNavigation"
                                             @click="logout"
                                             v-slot="{ active }"
                                         >
                                             <a
                                                 :class="[
-                                                    'block px-4 py-2 text-sm text-gray-700',
+                                                    'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
                                                 ]"
                                                 >Sign out<output></output
                                             ></a>
@@ -164,7 +163,6 @@
                     </div>
                     <div class="mt-3 space-y-1 px-2">
                         <DisclosureButton
-                            v-for="item in userNavigation"
                             @click="logout"
                             class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                             >Sign out</DisclosureButton
@@ -195,30 +193,21 @@ import { useRouter } from "vue-router";
 
 const user = {};
 const navigation = [
-    // { name: "Dashboard", href: "#", current: true },
     { name: "Dashboard", to: { name: "Dashboard" } },
-    // { name: "Team", href: "#", current: false },
     { name: "Surveys", to: { name: "Surveys" } },
-    // { name: "Projects", href: "#", current: false },
-    // { name: "Calendar", href: "#", current: false },
-    // { name: "Reports", href: "#", current: false },
 ];
 
 // logout function that will redirect to the login page
+const router = useRouter();
+const store = useStore();
 
 function logout() {
+    // will commit the mutations
     store.commit("logout");
     router.push({
         name: "Login",
     });
 }
-const userNavigation = [
-    // { name: "Your Profile", href: "#" },
-    // { name: "Settings", href: "#" },
-    { name: "Sign out", href: "#" },
-    // { nameL: "Sign out", to: { name: "Login" } },
-];
-const router = useRouter();
 </script>
 
 <style scoped></style>
